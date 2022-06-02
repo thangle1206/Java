b349d7103148311773cdc91d629f6c7a491c5ca3
thangle1206 <106686277+thangle1206@users.noreply.github.com>
2022-06-02 22:01:57 +0700

    Add files via upload

--- src/test/java/com/thealgorithms/strings/UpperTest.java
+++ src/test/java/com/thealgorithms/strings/UpperTest.java
@@ -4,14 +4,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UpperTest {
+public class UpperTest{
     @Test
-    public void toUpperCase() {
-        String input1 = "hello world";
-        String input2 = "hElLo WoRlD";
-        String input3 = "HELLO WORLD";
-        assertEquals("HELLO WORLD", Upper.toUpperCase(input1));
-        assertEquals("HELLO WORLD", Upper.toUpperCase(input2));
-        assertEquals("HELLO WORLD", Upper.toUpperCase(input3));
+    public void toUpperCase1() {
+        String str1 = "sOfTware TEstinG";
+        assertEquals("SOFTWARE TESTING", Upper.toUpperCase(str1));
+    }
+
+    @Test
+    public void toUpperCase2() {
+        String str2 = "hEllO WoRLD";
+        assertEquals("HELLO WORLD", Upper.toUpperCase(str2));
+    }
+
+    @Test
+    public void toUpperCase3() {
+        String str3 = "i aM Thang";
+        assertEquals("I AM THANG", Upper.toUpperCase(str3));
     }
 }
